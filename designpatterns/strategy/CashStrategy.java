2bef11abdb539e791d610c025c773ecb3aad47f9
Saranga B <bsaranga@gmail.com>
2024-12-25 13:14:03 +0530

    Enhance CashStrategy to support multiple currencies and update PaymentProcessor for dynamic strategy assignment

--- designpatterns/strategy/CashStrategy.java
+++ designpatterns/strategy/CashStrategy.java
@@ -2,8 +2,23 @@ package designpatterns.strategy;
 
 public class CashStrategy implements PaymentStrategy {
     
+    private String currency;
+
+    public CashStrategy() {
+        this.currency = "USD";
+    }
+
+    public CashStrategy(String currency) {
+        this.currency = currency;
+    }
+
     @Override
     public void pay(float amount) {
+        if (currency.equals("USD"))
+            System.out.println("Paying " + "$" + amount + " using cash.");
+        else if (currency.equals("Rupees"))
+            System.out.println("Paying " + "₹" + amount + " using cash.");
+        else
         System.out.println("Paying " + amount + " using cash.");
     }
 }
--- designpatterns/strategy/PaymentProcessor.java
+++ designpatterns/strategy/PaymentProcessor.java
@@ -1,16 +1,21 @@
 package designpatterns.strategy;
 
 public class PaymentProcessor {
+    // holds a reference to an instance of the payment strategy
     private PaymentStrategy paymentStrategy;
 
+    // we create the payment processor using an externally provided
+    // payment strategy, either cash or credit card
     public PaymentProcessor(PaymentStrategy paymentStrategy) {
         this.paymentStrategy = paymentStrategy;
     }
 
+    // we can set the payment strategy at runtime if we need
     public void setPaymentStrategy(PaymentStrategy paymentStrategy) {
         this.paymentStrategy = paymentStrategy;
     }
 
+    // calls the pay method of the selected payment strategy
     public void processPayment(float amount) {
         paymentStrategy.pay(amount);
     }
