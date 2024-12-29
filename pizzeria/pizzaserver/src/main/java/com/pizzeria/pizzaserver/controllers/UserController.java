84392cf633529f942fef65ed6e5321b83cd597f5
Saranga B <bsaranga@gmail.com>
2024-12-29 15:47:55 +0530

    Remove unused HttpStatus imports from UserController

--- pizzeria/pizzaserver/src/main/java/com/pizzeria/pizzaserver/controllers/UserController.java
+++ pizzeria/pizzaserver/src/main/java/com/pizzeria/pizzaserver/controllers/UserController.java
@@ -1,8 +1,6 @@
 package com.pizzeria.pizzaserver.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import com.pizzeria.pizzaserver.dto.UserDto;
